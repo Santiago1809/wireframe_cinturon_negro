2d499c3af92f42ac0a38f2ce2b4b20ef70090c71
Santiago1809 <aristizabalsantiago482@gmail.com>
2023-10-14 23:52:48 -0500

    Run.py fix

--- run.py
+++ run.py
@@ -1,8 +1,6 @@
 from werkzeug.security import check_password_hash
-
 from flask_app.app import app
-from flask_app.config.database import cursor
 
-if __name__ == '__main__':
- 
-  app.run(host='localhost', port=3001, debug=True)
+
+if __name__ == '__main__': 
+  app.run()
